3a845dc36ef7302d8e1073632acbdd0c6f1da3eb
iamgoeldhruv <goeldhruv9876@gmail.com>
2023-08-07 03:06:13 +0530

    Page linking done

---
+++ script.js
@@ -0,0 +1,21 @@
+// const button=document.getElementById("button");
+// const usercheck = true;
+// button.addEventListener(('click', function() {
+    
+ 
+  
+//     if (usercheck) {
+      
+//       window.location.href = 'index.html';
+//     } else {
+      
+//       console.log('User check is false.');
+//     }
+//   }));
+ 
+  
+  
+  
+  
+  
+  
