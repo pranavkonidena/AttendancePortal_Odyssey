f25c1403398d1aeaad9392c552646975f634c1a0
Sirius <pranav474738@gmail.com>
2023-08-07 04:06:31 +0530

    Final API is working

--- final.js
+++ final.js
@@ -6,24 +6,31 @@ var abi = [
     "function getAttendance(string _username, string _courseName) view returns (uint256)",
 ];
 
-var privateKey = "401db78ea27ea39c278ec601a6bd5c88fdd51cc20c38a3f20b05f827876dc83c";
+var privateKey = "20532186e8f8a5b5e7c0cc05f3bfc2a440d672c6d49d02c2b47c76248834fdcf";
 
 var signer = new ethers.Wallet(privateKey, provider);
 
-var provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/40589e1408fe41568be1972f63e5d3d1")
+var provider = new ethers.providers.JsonRpcProvider("https://eth-sepolia.g.alchemy.com/v2/zGJdOpgypgcR9vle_gJXaAvQT1mzmJGa")
 
 var address = "0x3BE5b393B88072a6b87A901db12A6b28A9F71C67";
 
 var read_contract = new ethers.Contract(address , abi ,provider);
-var write_contract = new ethers.Contract(address , abi , signer);
+// var write_contract = new ethers.Contract(address , abi , signer);
 
 const main = async () => {
 
     // To read
-    const isRegistered = await read_contract."methodHere";
-    //To write
-    let tx = await new_contract."Methodhere";
-    await tx.wait();
+    
+    // const data = await provider.getCode(address);
+    // const isRegistered = await write_contract.registerUser("god");
+    // const gasLimit = 100000;
+    // const wallet = new ethers.Wallet(privateKey, provider);
+    // var write_contract = new ethers.Contract(address , abi , wallet);
+    // const tx = await write_contract.connect(wallet).registerUser("god");
+    // await tx.wait();
+    const isRegistered = await read_contract.checkRegistration("god");
+    console.log(isRegistered);
+    
 }
 
 main();
